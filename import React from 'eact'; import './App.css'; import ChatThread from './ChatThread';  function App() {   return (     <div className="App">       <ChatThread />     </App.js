cd430244ea7ceb9cccf7371eaee2a39350104496
Mariakj <36694059+Mariakj@users.noreply.github.com>
2024-05-14 23:35:18 +0530

    Create App.js

---
+++ import React from 'eact'; import './App.css'; import ChatThread from './ChatThread';  function App() {   return (     <div className="App">       <ChatThread />     </App.js
@@ -0,0 +1,13 @@
+import React from 'eact';
+import './App.css';
+import ChatThread from './ChatThread';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatThread />
+    </div>
+  );
+}
+
+export default App;
